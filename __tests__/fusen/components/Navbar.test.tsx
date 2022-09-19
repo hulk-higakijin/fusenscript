a5b63317a6069dacad130a90362830674ba74b19
hulk-higakijin <hm385.chejptks@gmail.com>
2022-09-19 21:30:43 +0900

    test: Navbarのテストを追加

--- __tests__/fusen/components/Navbar.test.tsx
+++ __tests__/fusen/components/Navbar.test.tsx
@@ -1,7 +1,9 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { getPage } from 'next-page-tester'
 import Navbar from 'components/Layouts/Navbar'
 
-describe('Navbarコンポーネント', () => {
+describe('Navbar', () => {
   test('が機能すること', () => {
     const { getByText } = render(<Navbar />)
     expect(getByText('FusenScript')).toBeTruthy()

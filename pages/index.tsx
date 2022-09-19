4f2c9be9413823de7e6cbb8cee707f273b69e320
hulk-higakijin <hm385.chejptks@gmail.com>
2022-09-19 09:31:38 +0900

    chore: daisyuiの導入

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline text-blue-500">Hello world!</h1>;
+  return <h1 className="text-3xl font-bold underline text-primary">Hello world!</h1>;
 };
 
 export default Home;

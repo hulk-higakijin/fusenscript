3a53cf7e8431ae5912d5f4cb75944992b65f1630
hulk-higakijin <hm385.chejptks@gmail.com>
2022-09-19 22:39:17 +0900

    feat: room詳細ページにて、supabaseからroom情報を取得

--- pages/fusen/rooms/[uid].tsx
+++ pages/fusen/rooms/[uid].tsx
@@ -1,9 +1,23 @@
-import { NextPage } from 'next'
+import { GetServerSideProps, InferGetServerSidePropsType, NextPage } from 'next'
+import { useState } from 'react'
+import { supabase } from 'utils/supabase'
 
-const RoomsUidPage: NextPage = () => {
+type Props = InferGetServerSidePropsType<typeof getServerSideProps>
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const uid: string = context.query.uid as string
+  const { data: rooms } = await supabase.from('room').select('*').eq('uid', uid)
+  return {
+    props: { rooms },
+  }
+}
+
+const RoomsUidPage: NextPage<Props> = ({ rooms }) => {
+  const [room, setRoom] = useState<Room>(rooms[0])
+
   return (
     <>
-      <p>hogehoge</p>
+      <p>{room.name}</p>
     </>
   )
 }

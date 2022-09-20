d9d52611e6c347a11bc9acd34a0df5fc03064dcb
hulk-higakijin <hm385.chejptks@gmail.com>
2022-09-20 12:03:55 +0900

    feat: sessionの取得

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
+import { Session } from '@supabase/gotrue-js/src/lib/types'
 import type { AppProps } from 'next/app'
+import { useEffect, useState } from 'react'
 import Navbar from 'components/Layouts/Navbar'
+import { supabase } from 'utils/supabase'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [session, setSession] = useState<Session | null>(null)
+
+  useEffect(() => {
+    setSession(supabase.auth.session())
+  }, [])
+
+
   return (
     <>
       <Navbar />

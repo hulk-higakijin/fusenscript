423c775c0d6e3f1fef46e830b99e03d8405a891c
hulk-higakijin <hm385.chejptks@gmail.com>
2022-09-20 18:41:30 +0900

    feat: sessionを取得し、Auth.Providerで状態管理

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,20 @@
 import '../styles/globals.css'
 import { Session } from '@supabase/gotrue-js/src/lib/types'
 import type { AppProps } from 'next/app'
-import { useEffect, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
 import Navbar from 'components/Layouts/Navbar'
 import { supabase } from 'utils/supabase'
 
+export const Auth = createContext<AuthContextType>({ session: {} })
+
 function MyApp({ Component, pageProps }: AppProps) {
-  const [session, setSession] = useState<Session | null>(null)
-
-  useEffect(() => {
-    setSession(supabase.auth.session())
-  }, [])
-
+  const [session] = useState(supabase.auth.session())
 
   return (
-    <>
+    <Auth.Provider value={{ session }}>
       <Navbar />
       <Component {...pageProps} />
-    </>
+    </Auth.Provider>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
+import { Session } from '@supabase/gotrue-js/src/lib/types'
 import type { NextPage } from 'next'
 import Link from 'next/link'
+import { useContext } from 'react'
+import { Auth } from './_app'
 
 const Home: NextPage = () => {
+  const { session } = useContext<AuthContextType>(Auth)
+  console.log(session)
+
   return (
     <>
-      <h1 className='text-3xl font-bold underline text-primary'>
-        Hello world!
-      </h1>
+      <h1 className='text-3xl font-bold underline text-primary'></h1>
       <Link href={'/fusen/rooms'}>Play start</Link>
     </>
   )

c8136f1d0968951d70a1418ef22903e09c936145
hulk-higakijin <hm385.chejptks@gmail.com>
2022-09-22 09:53:30 +0900

    feat: room詳細ページに、フセン・カンバンを出力

--- pages/fusen/rooms/[uid].tsx
+++ pages/fusen/rooms/[uid].tsx
@@ -7,17 +7,38 @@ type Props = InferGetServerSidePropsType<typeof getServerSideProps>
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const uid: string = context.query.uid as string
   const { data: rooms } = await supabase.from('room').select('*').eq('uid', uid)
+  const { data: fusens } = await supabase
+    .from('fusen')
+    .select('*')
+    .eq('room_id', uid)
+  const { data: kanbans } = await supabase
+    .from('kanban')
+    .select('*')
+    .eq('room_id', uid)
   return {
-    props: { rooms },
+    props: { rooms, fusens, kanbans },
   }
 }
 
-const RoomsUidPage: NextPage<Props> = ({ rooms }) => {
+const RoomsUidPage: NextPage<Props> = ({ rooms, fusens, kanbans }) => {
   const [room, setRoom] = useState<Room>(rooms[0])
+  console.log(kanbans)
 
   return (
     <>
       <p>{room.name}</p>
+      <p>--------------------</p>
+      <ul>
+        {fusens.map((fusen: Fusen) => (
+          <li key={fusen.id}>{fusen.content}</li>
+        ))}
+      </ul>
+      <p>--------------------</p>
+      <ul>
+        {kanbans.map((kanban: Kanban) => (
+          <li key={kanban.id}>{kanban.title}</li>
+        ))}
+      </ul>
     </>
   )
 }
--- types/room.d.ts
+++ types/room.d.ts
@@ -4,3 +4,22 @@ type Room = {
   name: string
   created_at: string
 }
+
+type Fusen = {
+  id: string
+  content: string
+  xcoordinate: number
+  ycoordinate: number
+  user_id: string
+  room_id: string
+  created_at: string
+}
+
+type Kanban = {
+  id: string
+  title: string
+  xcoordinate: number
+  ycoordinate: number
+  room_id: string
+  created_at: string
+}

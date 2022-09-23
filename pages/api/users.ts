22fcfb048e9515e1c3d1b898daf1e117010cf82c
hulk-higakijin <hm385.chejptks@gmail.com>
2022-09-23 22:10:07 +0900

    refactor: prettier実行

--- pages/api/users.ts
+++ pages/api/users.ts
@@ -1,10 +1,10 @@
-import { users } from '@clerk/clerk-sdk-node';
+import { users } from '@clerk/clerk-sdk-node'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
-  ) {
+) {
   const data = await users.getUserList()
   res.status(200).json(data)
 }

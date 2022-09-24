437e1db93f0913109fd218c2eb4c812525d97306
hulk-higakijin <hm385.chejptks@gmail.com>
2022-09-24 18:00:19 +0900

    refactor: roomNameコンポーネントの作成

---
+++ components/Layouts/fusen/Room/Name.tsx
@@ -0,0 +1,66 @@
+import { useRouter } from 'next/router'
+import { FormEvent, useState } from 'react'
+import { supabase } from 'utils/supabase'
+
+const RoomName = ({ room }: { room: Room }) => {
+  const [defaultRoomName, setDefaultRoomName] = useState<string>(room.name)
+  const [inputRoomName, setInputRoomName] = useState<string>(room.name)
+  const [showForm, setShowForm] = useState<boolean>(false)
+  const router = useRouter()
+  const { uid } = router.query
+
+  const handleUpdate = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const { data } = await supabase
+      .from('room')
+      .update([{ name: inputRoomName }])
+      .match({ uid: uid })
+      .single()
+    setDefaultRoomName(data.name)
+    setShowForm(false)
+  }
+
+  const handleDelete = async () => {
+    await supabase.from('room').delete().match({ uid: uid })
+    alert('部屋を削除します。')
+    router.push('/fusen/rooms')
+  }
+
+  return (
+    <>
+      {showForm ? (
+        <form onSubmit={(e) => handleUpdate(e)}>
+          <input
+            type='text'
+            value={inputRoomName}
+            className='input input-bordered'
+            onChange={(e) => setInputRoomName(e.target.value)}
+            required
+            pattern='.*[^\s]+.*'
+          />
+          <button type='submit' className='btn'>
+            登録
+          </button>
+          <button
+            onClick={() => setShowForm(false)}
+            className='btn btn-primary'
+          >
+            キャンセル
+          </button>
+        </form>
+      ) : (
+        <>
+          <p>{defaultRoomName}</p>
+          <button className='btn' onClick={() => setShowForm(true)}>
+            編集
+          </button>
+          <button className='btn btn-primary' onClick={() => handleDelete()}>
+            削除
+          </button>
+        </>
+      )}
+    </>
+  )
+}
+
+export default RoomName
--- pages/fusen/rooms/[uid].tsx
+++ pages/fusen/rooms/[uid].tsx
@@ -1,8 +1,7 @@
 import { GetServerSideProps, InferGetServerSidePropsType, NextPage } from 'next'
-import { useRouter } from 'next/router'
-import { FormEvent, useState } from 'react'
 import Fusens from 'components/Layouts/fusen/Fusens'
 import Kanbans from 'components/Layouts/fusen/Kanbans'
+import RoomName from 'components/Layouts/fusen/Room/Name'
 import { supabase } from 'utils/supabase'
 
 type Props = InferGetServerSidePropsType<typeof getServerSideProps>
@@ -27,73 +26,14 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   )
 
   return {
-    props: { room, fusens, kanbans, users, uid },
+    props: { room, fusens, kanbans, users },
   }
 }
 
-const RoomsUidPage: NextPage<Props> = ({
-  room,
-  fusens,
-  kanbans,
-  users,
-  uid,
-}) => {
-  const [defaultRoomName, setDefaultRoomName] = useState<string>(room.name)
-  const [inputRoomName, setInputRoomName] = useState<string>(room.name)
-  const [showForm, setShowForm] = useState<boolean>(false)
-  const router = useRouter()
-
-  const handleUpdate = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    const { data } = await supabase
-      .from('room')
-      .update([{ name: inputRoomName }])
-      .match({ uid: uid })
-      .single()
-    setDefaultRoomName(data.name)
-    setShowForm(false)
-  }
-
-  const handleDelete = async () => {
-    await supabase.from('room').delete().match({ uid: uid })
-    alert('部屋を削除します。')
-    router.push('/fusen/rooms')
-  }
-
+const RoomsUidPage: NextPage<Props> = ({ room, fusens, kanbans, users }) => {
   return (
     <>
-      {showForm ? (
-        <form onSubmit={(e) => handleUpdate(e)}>
-          <input
-            type='text'
-            value={inputRoomName}
-            className='input input-bordered'
-            onChange={(e) => setInputRoomName(e.target.value)}
-            required
-            pattern='.*[^\s]+.*'
-          />
-          <button type='submit' className='btn'>
-            登録
-          </button>
-          <button
-            onClick={() => setShowForm(false)}
-            className='btn btn-primary'
-          >
-            キャンセル
-          </button>
-        </form>
-      ) : (
-        <>
-          <p>{defaultRoomName}</p>
-          <button className='btn' onClick={() => setShowForm(true)}>
-            編集
-          </button>
-          <button className='btn btn-primary' onClick={() => handleDelete()}>
-            削除
-          </button>
-        </>
-      )}
-
+      <RoomName room={room} />
       <p>--------------------</p>
       <Fusens fusens={fusens} users={users} />
       <p>--------------------</p>

e416d6085425cc0a93a1fd86a740615d53de9f99
hulk-higakijin <hm385.chejptks@gmail.com>
2022-09-24 23:53:11 +0900

    feat: fusenの座標情報の更新処理の実装

--- components/Layouts/fusen/Kanban.tsx
+++ components/Layouts/fusen/Kanban.tsx
@@ -1,16 +1,30 @@
 import { useState } from 'react'
-import Draggable, { DraggableEvent } from 'react-draggable'
+import Draggable, { DraggableData, DraggableEvent } from 'react-draggable'
+import { supabase } from 'utils/supabase'
 
 const Kanban = ({ kanban }: { kanban: Kanban }) => {
   const [color, setColor] = useState('bg-green-500')
-  const handleStop = (e: DraggableEvent) => {
-    console.log(e)
+  const [location, setLocation] = useState<Coordinate>({
+    xcoordinate: kanban.xcoordinate,
+    ycoordinate: kanban.ycoordinate,
+  })
+
+  const handleDrag = (e: DraggableEvent, data: DraggableData) => {
+    setLocation({ xcoordinate: data.x, ycoordinate: data.y })
+  }
+
+  const handleStop = () => {
+    const updateKanban = async () => {
+      await supabase.from('kanban').update(location).match({ id: kanban.id })
+    }
+    updateKanban()
   }
 
   return (
     <Draggable
       defaultPosition={{ x: kanban.xcoordinate, y: kanban.ycoordinate }}
-      onStop={(e) => handleStop(e)}
+      onDrag={(e, data) => handleDrag(e, data)}
+      onStop={() => handleStop()}
     >
       <div
         className={`${color} w-fit px-4 py-1 rounded-sm text-white cursor-grab absolute`}

30aa3fdd6f0aab71588a2f76ec5921eb90cc0827
hulk-higakijin <hm385.chejptks@gmail.com>
2022-09-24 21:25:47 +0900

    chore: draggableをfusen,kanbanに適応

--- components/Layouts/fusen/Fusen.tsx
+++ components/Layouts/fusen/Fusen.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Draggable from 'react-draggable'
 
 const Fusen = ({ fusen, users }: { fusen: Fusen; users: User[] }) => {
   const [userName, setUserName] = useState<string>(
@@ -7,12 +8,14 @@ const Fusen = ({ fusen, users }: { fusen: Fusen; users: User[] }) => {
   const [color, setColor] = useState('bg-blue-500') // ランダムカラーを設定したい
 
   return (
-    <div
-      className={`${color} h-28 w-40 text-white p-2 rounded-sm flex flex-col border border-black`}
-    >
-      <p className='m-auto'>{fusen.content}</p>
-      <p className='text-sm ml-auto'>{userName}</p>
-    </div>
+    <Draggable>
+      <div
+        className={`${color} h-28 w-40 text-white p-2 rounded-sm flex flex-col border border-black cursor-grab`}
+      >
+        <p className='m-auto'>{fusen.content}</p>
+        <p className='text-sm ml-auto'>{userName}</p>
+      </div>
+    </Draggable>
   )
 }
 
--- components/Layouts/fusen/Kanban.tsx
+++ components/Layouts/fusen/Kanban.tsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
+import Draggable from 'react-draggable'
 
 const Kanban = ({ kanban }: { kanban: Kanban }) => {
   const [color, setColor] = useState('bg-green-500')
 
   return (
-    <div className={`${color} w-fit px-4 py-1 rounded-sm text-white`}>
-      {kanban.title}
-    </div>
+    <Draggable>
+      <div className={`${color} w-fit px-4 py-1 rounded-sm text-white cursor-grab`}>
+        {kanban.title}
+      </div>
+    </Draggable>
   )
 }
 

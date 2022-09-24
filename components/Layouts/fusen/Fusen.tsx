09dc2149cefac79d0e0ed16848f18c87d255d00a
hulk-higakijin <hm385.chejptks@gmail.com>
2022-09-24 23:19:22 +0900

    fix: fusenにおいて、onDragにより値を取得する

--- components/Layouts/fusen/Fusen.tsx
+++ components/Layouts/fusen/Fusen.tsx
@@ -1,20 +1,33 @@
 import { useState } from 'react'
-import Draggable, { DraggableEvent } from 'react-draggable'
+import Draggable, { DraggableData, DraggableEvent } from 'react-draggable'
+import { supabase } from 'utils/supabase'
 
 const Fusen = ({ fusen, users }: { fusen: Fusen; users: User[] }) => {
   const [userName, setUserName] = useState<string>(
     users.filter((user: User) => user.id == fusen.user_id)[0].firstName,
   )
   const [color, setColor] = useState('bg-blue-500') // ランダムカラーを設定したい
+  const [location, setLocation] = useState({
+    xcoordinate: fusen.xcoordinate,
+    ycoordinate: fusen.ycoordinate,
+  })
 
-  const handleStop = (e: DraggableEvent) => {
-    console.log(e)
+  const handleDrag = (e: DraggableEvent, data: DraggableData) => {
+    setLocation({ xcoordinate: data.x, ycoordinate: data.y })
+  }
+
+  const handleStop = () => {
+    const updateFusen = async () => {
+      await supabase.from('fusen').update(location).match({ id: fusen.id })
+    }
+    updateFusen()
   }
 
   return (
     <Draggable
-      defaultPosition={{x: fusen.xcoordinate, y: fusen.ycoordinate}}
-      onStop={(e) => handleStop(e)}
+      defaultPosition={{ x: fusen.xcoordinate, y: fusen.ycoordinate }}
+      onDrag={(e, data) => handleDrag(e, data)}
+      onStop={() => handleStop()}
     >
       <div
         className={`${color} h-28 w-40 text-white p-2 rounded-sm flex flex-col border border-black cursor-grab absolute`}

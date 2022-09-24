3272b0f5f83ec1b8ab5b1e926a6a478216e13ed4
hulk-higakijin <hm385.chejptks@gmail.com>
2022-09-24 21:26:24 +0900

    refactor: prettier修正

--- components/Layouts/fusen/Kanban.tsx
+++ components/Layouts/fusen/Kanban.tsx
@@ -6,7 +6,9 @@ const Kanban = ({ kanban }: { kanban: Kanban }) => {
 
   return (
     <Draggable>
-      <div className={`${color} w-fit px-4 py-1 rounded-sm text-white cursor-grab`}>
+      <div
+        className={`${color} w-fit px-4 py-1 rounded-sm text-white cursor-grab`}
+      >
         {kanban.title}
       </div>
     </Draggable>

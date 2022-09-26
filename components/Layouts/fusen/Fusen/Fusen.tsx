2d625ab82a3d31e5f8da60e1d8284ddd2de9b30e
hulk-higakijin <hm385.chejptks@gmail.com>
2022-09-26 20:58:55 +0900

    feat: fusen削除機能の実装

--- components/Layouts/fusen/Fusen/Fusen.tsx
+++ components/Layouts/fusen/Fusen/Fusen.tsx
@@ -1,5 +1,9 @@
-import { useState } from 'react'
+import { useUser } from '@clerk/nextjs'
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useContext, useState } from 'react'
 import Draggable, { DraggableData, DraggableEvent } from 'react-draggable'
+import { FusenRoomsContext } from 'pages/fusen/rooms/[uid]'
 import { supabase } from 'utils/supabase'
 
 const Fusen = ({ fusen, users }: { fusen: Fusen; users: User[] }) => {
@@ -11,6 +15,12 @@ const Fusen = ({ fusen, users }: { fusen: Fusen; users: User[] }) => {
     xcoordinate: fusen.xcoordinate,
     ycoordinate: fusen.ycoordinate,
   })
+  const { user } = useUser()
+  let { fusens, setFusens } = useContext(FusenRoomsContext)
+
+  const isMyFusen = () => {
+    return fusen.user_id == user?.id
+  }
 
   const handleDrag = (e: DraggableEvent, data: DraggableData) => {
     setLocation({ xcoordinate: data.x, ycoordinate: data.y })
@@ -23,6 +33,20 @@ const Fusen = ({ fusen, users }: { fusen: Fusen; users: User[] }) => {
     updateFusen()
   }
 
+  const handleDelete = () => {
+    const deleteFusen = async () => {
+      if (user) {
+        const { data } = await supabase
+          .from('fusen')
+          .delete()
+          .match({ id: fusen.id, user_id: user.id })
+          .single()
+        setFusens(fusens.filter((fusen: Fusen) => fusen.id !== data.id))
+      }
+    }
+    deleteFusen()
+  }
+
   return (
     <Draggable
       defaultPosition={{ x: fusen.xcoordinate, y: fusen.ycoordinate }}
@@ -30,8 +54,19 @@ const Fusen = ({ fusen, users }: { fusen: Fusen; users: User[] }) => {
       onStop={() => handleStop()}
     >
       <div
-        className={`${color} h-28 w-40 text-white p-2 rounded-sm flex flex-col border border-black cursor-grab absolute`}
+        className={`${color} h-28 w-40 text-white px-2 pb-2 rounded-sm flex flex-col border border-black cursor-grab absolute`}
       >
+        {isMyFusen() ? (
+          <div className='ml-auto'>
+            <FontAwesomeIcon
+              icon={faXmark}
+              className='hover:text-2xl hover:text-red-500 cursor-pointer'
+              onClick={() => handleDelete()}
+            />
+          </div>
+        ) : (
+          <></>
+        )}
         <p className='m-auto'>{fusen.content}</p>
         <p className='text-sm ml-auto'>{userName}</p>
       </div>

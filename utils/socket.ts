b0477c46f7436abac09a4497fd7d62ac430f31ab
hulk-higakijin <hm385.chejptks@gmail.com>
2022-10-01 17:48:05 +0900

    feat: socketにおける環境変数の設定

--- utils/socket.ts
+++ utils/socket.ts
@@ -1,4 +1,4 @@
 import { io, Socket } from 'socket.io-client'
 
-const server_url = 'http://localhost:8000'
+const server_url = process.env.NEXT_PUBLIC_SOCKET_URL as string
 export const socket: Socket = io(server_url)

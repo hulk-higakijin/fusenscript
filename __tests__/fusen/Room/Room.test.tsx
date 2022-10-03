bbb642c38dcb7bd014de0180f056dfddf5e6baf4
hulk-higakijin <hm385.chejptks@gmail.com>
2022-10-03 23:03:00 +0900

    test: roomコンポーネントのテストのおいて、テスト項目名を修正

--- __tests__/fusen/Room/Room.test.tsx
+++ __tests__/fusen/Room/Room.test.tsx
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react'
 import Room from 'components/Layouts/fusen/Room/Room'
 
-describe('Roomコンポーネントに関するテスト', () => {
+describe('Roomコンポーネントが', () => {
   const room: Room = {
     id: '1',
     uid: 'room1',
@@ -9,7 +9,7 @@ describe('Roomコンポーネントに関するテスト', () => {
     created_at: '2020/10/03',
   }
 
-  test('が機能すること', () => {
+  test('機能すること', () => {
     const { getByText } = render(<Room {...{ room: room }} />)
     expect(getByText('our first room')).toBeTruthy()
   })
